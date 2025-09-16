6f1d7f21f840da2cd479467da40c3fb424db4e0b
eyzaun <eyzaun@users.noreply.github.com>
2025-09-16 19:08:48 +0300

    chore: remove redundant BAT scripts and stray binaries; keep build.bat and run-godash.bat only

--- get_processes.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-cd o:\Web_Projects\godash\godash
-start /b go run main.go
-timeout /t 5 /nobreak > nul
-curl -s "http://localhost:8080/api/v1/metrics/processes" > processes_response_final.json
-type processes_response_final.json
