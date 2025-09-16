45d4ed7d4e489abfb82b583c5243445b1eee6f1e
eyzaun <eyzaun@users.noreply.github.com>
2025-09-16 19:59:23 +0300

    chore: repo cleanup  remove unused frontend/wails demos, sample JSONs, local DB and scripts; tighten .gitignore; drop duplicate embed file

--- cpu_load.ps1
+++
@@ -1,5 +0,0 @@
-while ($true) {
-    1..1000000 | ForEach-Object { $null }
-    Write-Host "Generating CPU load..."
-    Start-Sleep -Milliseconds 100
-}
--- svg-demo/go.mod
+++
@@ -1,5 +0,0 @@
-module svg-charts
-
-go 1.24.5
-
-require github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b
